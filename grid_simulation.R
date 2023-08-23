439e815a91e53c3400988b7ebd65c3c1671efe2a
Philippe Tadger <philippetadger@gmail.com>
2023-08-23 09:39:03 +0200

    Delete grid_simulation.R

--- grid_simulation.R
+++
@@ -1,6 +0,0 @@
-expand.grid
-library(dplyr)
-tablita<-expand.grid(Day = c(2, 3),Sample = c(1,2,3),NxSchool = 50,TP = c(1,2,3),LikeR = c(1.5,3))
-tablita$Scenario<-rownames(tablita)
-tablita<-tablita[,c(6,1:5)]
-tablita%>%flextable::flextable()
